8e613c5d398a623413c0d6f6d146ca73221bb5a1
Igorg599 <igorg599@gmail.com>
2021-04-20 16:53:15 +0500

    work with forms

--- src/components/shape/RectangularShape.tsx
+++ src/components/shape/RectangularShape.tsx
@@ -1,125 +1,186 @@
-import React from 'react';
-import {useTranslation} from "react-i18next";
-import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import BigVerticalArrow from '../../assets/icons/BigVerticalArrow.svg';
-import BigHorizontalArrow from '../../assets/icons/BigHorizontalArrow.svg';
-import { Shape, Line, Figure, SecondArrow, ThirdArrow, Result, Form, theme} from './styles';
-import { ThemeProvider } from '@material-ui/core/styles';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import BigVerticalArrow from "../../assets/icons/BigVerticalArrow.svg";
+import BigHorizontalArrow from "../../assets/icons/BigHorizontalArrow.svg";
+import {
+  Shape,
+  Line,
+  Figure,
+  SecondArrow,
+  ThirdArrow,
+  Result,
+  Form,
+  theme,
+} from "./styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 
 interface PropsTab {
-    active: number
+  active: number;
 }
 
-function RectangularShape({active}: PropsTab) {
-    const {t} = useTranslation();
+function RectangularShape({ active }: PropsTab) {
+  const { t } = useTranslation();
 
-    const [calculation, setCalculation] = React.useState<{
-        square: string,
-        perimeter: string,
-        bigHeight: number,
-        width: number
-    }>({
-        square: '0',
-        perimeter: '0',
-        bigHeight: 0,
-        width: 0
-    })
+  const [calculation, setCalculation] = React.useState<{
+    square: string;
+    perimeter: string;
+    bigHeight: number;
+    width: number;
+  }>({
+    square: "0",
+    perimeter: "0",
+    bigHeight: 0,
+    width: 0,
+  });
 
-    const [stateFocus, setStateFocus] = React.useState<{
-        focusInputHeight: boolean,
-        focusInputWidth: boolean,
-    }>({
-        focusInputHeight: false,
-        focusInputWidth: false
-    })
+  const [stateFocus, setStateFocus] = React.useState<{
+    focusInputHeight: boolean;
+    focusInputWidth: boolean;
+  }>({
+    focusInputHeight: false,
+    focusInputWidth: false,
+  });
 
-    const onValueBigHeight = (e: any) => {
-        setCalculation({...calculation, bigHeight: Math.abs(e.target.value)});
+  const onValueBigHeight = (e: any) => {
+    setCalculation({ ...calculation, bigHeight: Math.abs(e.target.value) });
+  };
+
+  const onValueWidth = (e: any) => {
+    setCalculation({ ...calculation, width: Math.abs(e.target.value) });
+  };
+
+  if (!calculation.bigHeight || !calculation.width) {
+    calculation.square = "0";
+    calculation.perimeter = "0";
+  } else {
+    calculation.square = (
+      Math.ceil(calculation.bigHeight * calculation.width * 10) / 10
+    )
+      .toString()
+      .replace(".", ",");
+    calculation.perimeter = (
+      Math.ceil((calculation.bigHeight * 2 + calculation.width * 2) * 10) / 10
+    )
+      .toString()
+      .replace(".", ",");
+  }
+
+  function onFocusWidth() {
+    setStateFocus({ ...stateFocus, focusInputWidth: true });
+  }
+
+  function onBlurWidth() {
+    setStateFocus({ ...stateFocus, focusInputWidth: false });
+  }
+
+  function onFocusHeight() {
+    setStateFocus({ ...stateFocus, focusInputHeight: true });
+  }
+
+  function onBlurHeight() {
+    setStateFocus({ ...stateFocus, focusInputHeight: false });
+  }
+
+  const onKeyDownWidth = (e: any) => {
+    if (e.keyCode === 13 && e.target.value) {
+      e.target.blur();
+      if (!calculation.bigHeight) {
+        document.getElementById("rectangular_height")?.focus();
+      }
     }
+  };
 
-    const onValueWidth = (e: any) => {
-        setCalculation({...calculation, width: Math.abs(e.target.value)});
+  const onKeyDownHeight = (e: any) => {
+    if (e.keyCode === 13 && e.target.value) {
+      e.target.blur();
+      if (!calculation.width) {
+        document.getElementById("rectangular_width")?.focus();
+      }
     }
+  };
 
-    if (!calculation.bigHeight || !calculation.width) {
-        calculation.square = '0';
-        calculation.perimeter = '0';
-    } else {
-        calculation.square = (Math.ceil((calculation.bigHeight * calculation.width) * 10) / 10).toString().replace('.', ',');
-        calculation.perimeter = (Math.ceil((calculation.bigHeight * 2 + calculation.width * 2) * 10) / 10).toString().replace('.', ',');
-    }
-
-    function onFocusWidth() {
-        setStateFocus({...stateFocus, focusInputWidth: true})
-    }
-
-    function onBlurWidth() {
-        setStateFocus({...stateFocus, focusInputWidth: false})
-    }
-
-    function onFocusHeight() {
-        setStateFocus({...stateFocus, focusInputHeight: true})
-    }
-
-    function onBlurHeight() {
-        setStateFocus({...stateFocus, focusInputHeight: false})
-    }
-
-    return (
-      <Shape style={ active === 0 || active === 2 ? {  display: "block" } : {  display: "none" }}>
-        <h2>{t('rectangular_shape')}</h2>
-        <Line/>
-        <Figure>
-            <Form radius="0">
-                <ThirdArrow src={BigHorizontalArrow} style={{ marginBottom: 20 }} alt="arrow"/>
-                <ThemeProvider theme={theme}>
-                    <TextField
-                        label={t('width')}
-                        variant="outlined"
-                        type='number'
-                        value={calculation.width === 0 ? '' : calculation.width}
-                        onChange={onValueWidth}
-                        style={{ width: 112, marginLeft: 1 }}
-                        onFocus={onFocusWidth}
-                        onBlur={onBlurWidth}
-                        InputProps={{
-                            endAdornment: <InputAdornment position="end">{stateFocus.focusInputWidth || calculation.width ? t('meters') : ''}</InputAdornment>,
-                        }}
-                    />
-                </ThemeProvider>
-            </Form>
-            <SecondArrow top="2px" src={BigVerticalArrow} alt="arrow"/>
-            <ThemeProvider theme={theme}>
-                <TextField
-                    label={t('height')}
-                    value={calculation.bigHeight === 0 ? '' : calculation.bigHeight}
-                    onChange={onValueBigHeight}
-                    type='number'
-                    variant="outlined"
-                    style={{ width: 112, top: 54, marginLeft: 16 }}
-                    onFocus={onFocusHeight}
-                    onBlur={onBlurHeight}
-                    InputProps={{
-                        endAdornment: <InputAdornment position="end">{stateFocus.focusInputHeight || calculation.bigHeight ? t('meters') : ''}</InputAdornment>,
-                    }}
-                />
-            </ThemeProvider>
-        </Figure>
-        <Result>
-            <div>
-                <h3>{t('area')}</h3>
-                <p>{calculation.square}</p>
-                <h4>{t('square_meters')}</h4>
-            </div>
-            <div style={{ marginLeft: 67 }}>
-                <h3>{t('perimeter')}</h3>
-                <p>{calculation.perimeter}</p>
-                <h4>{t('meters')}</h4>
-            </div>
-        </Result>
-      </Shape>
-    );
+  return (
+    <Shape
+      style={
+        active === 0 || active === 2
+          ? { display: "block" }
+          : { display: "none" }
+      }
+    >
+      <h2>{t("rectangular_shape")}</h2>
+      <Line />
+      <Figure>
+        <Form radius="0">
+          <ThirdArrow
+            src={BigHorizontalArrow}
+            style={{ marginBottom: 20 }}
+            alt="arrow"
+          />
+          <ThemeProvider theme={theme}>
+            <TextField
+              id="rectangular_width"
+              label={t("width")}
+              variant="outlined"
+              type="number"
+              value={calculation.width === 0 ? "" : calculation.width}
+              onChange={onValueWidth}
+              style={{ width: 112, marginLeft: 1 }}
+              onKeyUp={onKeyDownWidth}
+              onFocus={onFocusWidth}
+              onBlur={onBlurWidth}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    {stateFocus.focusInputWidth || calculation.width
+                      ? t("meters")
+                      : ""}
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </ThemeProvider>
+        </Form>
+        <SecondArrow top="2px" src={BigVerticalArrow} alt="arrow" />
+        <ThemeProvider theme={theme}>
+          <TextField
+            id="rectangular_height"
+            label={t("height")}
+            value={calculation.bigHeight === 0 ? "" : calculation.bigHeight}
+            onChange={onValueBigHeight}
+            onKeyUp={onKeyDownHeight}
+            type="number"
+            variant="outlined"
+            style={{ width: 112, top: 54, marginLeft: 16 }}
+            onFocus={onFocusHeight}
+            onBlur={onBlurHeight}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  {stateFocus.focusInputHeight || calculation.bigHeight
+                    ? t("meters")
+                    : ""}
+                </InputAdornment>
+              ),
+            }}
+          />
+        </ThemeProvider>
+      </Figure>
+      <Result>
+        <div>
+          <h3>{t("area")}</h3>
+          <p>{calculation.square}</p>
+          <h4>{t("square_meters")}</h4>
+        </div>
+        <div style={{ marginLeft: 67 }}>
+          <h3>{t("perimeter")}</h3>
+          <p>{calculation.perimeter}</p>
+          <h4>{t("meters")}</h4>
+        </div>
+      </Result>
+    </Shape>
+  );
 }
-  
+
 export default RectangularShape;
--- src/components/shape/RoundShape.tsx
+++ src/components/shape/RoundShape.tsx
@@ -1,88 +1,123 @@
-import React from 'react';
-import {useTranslation} from "react-i18next";
-import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import BigVerticalArrow from '../../assets/icons/BigVerticalArrow.svg';
-import { Shape, Line, Figure, FirstArrow, Form, Result, theme} from './styles';
-import { ThemeProvider } from '@material-ui/core/styles';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import BigVerticalArrow from "../../assets/icons/BigVerticalArrow.svg";
+import { Shape, Line, Figure, FirstArrow, Form, Result, theme } from "./styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 
 interface PropsTab {
-    active: number
+  active: number;
 }
 
-function RoundShape({active}: PropsTab) {
-    const {t} = useTranslation();
+function RoundShape({ active }: PropsTab) {
+  const { t } = useTranslation();
 
-    const [calculation, setCalculation] = React.useState<{
-        square: string,
-        perimeter: string,
-        diameter: number
-    }>({
-        square: "0",
-        perimeter: "0",
-        diameter: 0
-    })
+  const [calculation, setCalculation] = React.useState<{
+    square: string;
+    perimeter: string;
+    diameter: number;
+  }>({
+    square: "0",
+    perimeter: "0",
+    diameter: 0,
+  });
 
-    const [stateFocus, setStateFocus] = React.useState<{
-        focusInputDiameter: Boolean
-    }>({
-        focusInputDiameter: false
-    })
+  const [stateFocus, setStateFocus] = React.useState<{
+    focusInputDiameter: Boolean;
+  }>({
+    focusInputDiameter: false,
+  });
 
-    const onValueDiameter = (e: any) => {
-        setCalculation({...calculation, diameter: Math.abs(e.target.value.replace(/,/g, ''))});
+  const onValueDiameter = (e: any) => {
+    setCalculation({
+      ...calculation,
+      diameter: Math.abs(e.target.value.replace(/,/g, "")),
+    });
+  };
+
+  calculation.square = (
+    Math.ceil(
+      (calculation.diameter / 2) *
+        (calculation.diameter / 2) *
+        3.1415926535 *
+        10
+    ) / 10
+  )
+    .toString()
+    .replace(".", ",");
+
+  calculation.perimeter = (
+    Math.ceil(((2 * 3.1415926535 * calculation.diameter) / 2) * 10) / 10
+  )
+    .toString()
+    .replace(".", ",");
+
+  function onFocusDiameter() {
+    setStateFocus({ ...stateFocus, focusInputDiameter: true });
+  }
+
+  function onBlurDiameter() {
+    setStateFocus({ ...stateFocus, focusInputDiameter: false });
+  }
+
+  const onKeyDownDiameter = (e: any) => {
+    if (e.keyCode === 13 && e.target.value) {
+      e.target.blur();
     }
+  };
 
-    calculation.square = (Math.ceil(((calculation.diameter / 2 ) * (calculation.diameter / 2 ) * 3.1415926535) * 10) / 10).toString().replace('.', ',');
-
-    calculation.perimeter = (Math.ceil((2 * 3.1415926535 * calculation.diameter / 2) * 10) / 10).toString().replace('.', ',');
-
-    function onFocusDiameter() {
-        setStateFocus({...stateFocus, focusInputDiameter: true})
-    }
-
-    function onBlurDiameter() {
-        setStateFocus({...stateFocus, focusInputDiameter: false})
-    }
-
-    return (
-      <Shape style={ active === 0 || active === 3 ? {  display: "block" } : {  display: "none" }}>
-        <h2>{t('round_shape')}</h2>
-        <Line/>
-        <Figure>
-            <Form radius="84px">
-                <FirstArrow src={BigVerticalArrow} alt="arrow"/>
-                <ThemeProvider theme={theme}>
-                    <TextField
-                        label={t('diameter')}
-                        value={calculation.diameter === 0 ? '' : calculation.diameter}
-                        onChange={onValueDiameter}
-                        type='number'
-                        variant="outlined"
-                        style={{ width: 112, marginBottom: 6 }}
-                        onFocus={onFocusDiameter}
-                        onBlur={onBlurDiameter}
-                        InputProps={{
-                            endAdornment: <InputAdornment position="end">{stateFocus.focusInputDiameter || calculation.diameter ? t('meters') : ''}</InputAdornment>,
-                        }}
-                    />
-                </ThemeProvider>
-            </Form>
-        </Figure>
-        <Result>
-            <div>
-                <h3>{t('area')}</h3>
-                <p>{calculation.square}</p>
-                <h4>{t('square_meters')}</h4>
-            </div>
-            <div style={{ marginLeft: 67 }}>
-                <h3>{t('perimeter')}</h3>
-                <p>{calculation.perimeter}</p>
-                <h4>{t('meters')}</h4>
-            </div>
-        </Result>
-      </Shape>
-    );
+  return (
+    <Shape
+      style={
+        active === 0 || active === 3
+          ? { display: "block" }
+          : { display: "none" }
+      }
+    >
+      <h2>{t("round_shape")}</h2>
+      <Line />
+      <Figure>
+        <Form radius="84px">
+          <FirstArrow src={BigVerticalArrow} alt="arrow" />
+          <ThemeProvider theme={theme}>
+            <TextField
+              onKeyUp={onKeyDownDiameter}
+              label={t("diameter")}
+              value={calculation.diameter === 0 ? "" : calculation.diameter}
+              onChange={onValueDiameter}
+              type="number"
+              variant="outlined"
+              style={{ width: 112, marginBottom: 6 }}
+              onFocus={onFocusDiameter}
+              onBlur={onBlurDiameter}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    {stateFocus.focusInputDiameter || calculation.diameter
+                      ? t("meters")
+                      : ""}
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </ThemeProvider>
+        </Form>
+      </Figure>
+      <Result>
+        <div>
+          <h3>{t("area")}</h3>
+          <p>{calculation.square}</p>
+          <h4>{t("square_meters")}</h4>
+        </div>
+        <div style={{ marginLeft: 67 }}>
+          <h3>{t("perimeter")}</h3>
+          <p>{calculation.perimeter}</p>
+          <h4>{t("meters")}</h4>
+        </div>
+      </Result>
+    </Shape>
+  );
 }
-  
+
 export default RoundShape;
